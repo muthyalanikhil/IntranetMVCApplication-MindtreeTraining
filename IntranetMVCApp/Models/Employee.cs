e95e1b06b9658cdd7eb2518225c7912d80194960
nikhil muthyala <s528754@nwmissouri.edu>
2018-02-16 20:48:00 -0600

    uploading to github

---
+++ IntranetMVCApp/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntranetMVCApp.Models
+{
+    public class Employee
+    {
+        public int id;
+        public int Id { get; set; }
+
+    }
+}
